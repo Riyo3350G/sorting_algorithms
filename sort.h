ac06082fcaf92d153191293a6f57f41a2548cc3b
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-07-23 01:02:40 +0100

    Fix header

--- sort.h
+++ sort.h
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size);
 
 /* quick sort */
 void swap(int *a, int *b);
-int partition(int *arr, int first, int last, size_t size);
+int partition(int *arr, int first_elem, int last_elem, size_t size);
 void quick_sort_recursive(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
 
